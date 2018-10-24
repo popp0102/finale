f9e55b457f04fd41ab2b7e63d285ec4f6a6113bb
jpoppler <jason@getnotion.com>
2018-10-24 09:25:14 -0600

    use openstruct for shipments

--- lib/finale/shipment.rb
+++ lib/finale/shipment.rb
@@ -1,11 +1,5 @@
 module Finale
-  class Shipment
-    attr_reader :id, :item_list
-
-    def initialize(shipment_response)
-      @id        = shipment_response[:shipmentId]&.to_i
-      @item_list = shipment_response[:shipmentItemList]
-    end
+  class Shipment < OpenStruct
   end
 end
 
